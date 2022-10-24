4b25352ecab92c7f81148c7eab96570783cefd9f
aukras <42978607+aukras@users.noreply.github.com>
2022-10-24 19:25:48 +0300

    Create Secrets.java

---
+++ src/main/java/org/example/Secrets.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Secrets {
+    public static final String API_KEY = "add-your-api-key";
+    public static final String API_KEY_SECRET = "add-your-key-secret";
+    public static final String ACCESS_TOKEN = "add-your-access-token";
+    public static final String ACCESS_TOKEN_SECRET = "add-your-access-token-secret";
+}
